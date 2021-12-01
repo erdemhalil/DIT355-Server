5926bb905585ba7a4d922722342d330c34ef8c64
EnisBourbia <enis.bourbia@hotmail.com>
2021-12-01 11:04:44 +0100

    models for appoitment bux fix

--- server/appointments/models.py
+++ server/appointments/models.py
@@ -6,8 +6,8 @@ from users.models import User
 # Create your models here.
 
 class Appointment(models.Model):
-    data: models.DateTimeField()
-    completed: models.BooleanField(default=False)
-    description: models.CharField(max_length=300)
-    dentist: models.ForeignKey(Dentist, on_delete=models.DO_NOTHING)
-    user: models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    description = models.CharField(max_length=300)
+    dentist = models.ForeignKey(Dentist, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
--- server/dentists/admin.py
+++ server/dentists/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Dentist)
+admin.site.register(Coordinate)
+admin.site.register(Openinghours)
